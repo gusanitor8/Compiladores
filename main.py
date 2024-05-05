ab6a980ede3c511df92144905a4d2d9f2c0c5daf
gusanitor8 <gusanitor8@gmail.com>
2024-05-05 12:03:26 -0600

    files para calificar

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from src.yalex.tokenGetter import run as token_getter_run
 
 
 def run():
-    tokens, productions, production_adress = yapar_run("./utils/yapar_files/slr-4.yalp")
-    yalex_tokens = set(token_getter_run("./utils/yalex_files/slr-4.yal"))
-
-    print(yalex_tokens)
-    print(tokens)
-
-    if not tokens.issubset(yalex_tokens):
-        print("Tokens missing in yapar file")
-        raise Exception
+    tokens, productions, production_adress = yapar_run("./utils/yapar_files/YAPar4.txt")
+    # yalex_tokens = set(token_getter_run("./utils/yalex_files/YALex2.txt"))
+    #
+    # print(yalex_tokens)
+    # print(tokens)
+    #
+    # if not tokens.issubset(yalex_tokens):
+    #     print("Tokens missing in yapar file")
+    #     raise Exception
 
     builder = Builder(productions, production_adress)
     index_set_dic, lr0 = builder.get_item_sets()
